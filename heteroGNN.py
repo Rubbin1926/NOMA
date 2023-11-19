4d7bf083f0aa0c57881ed1eb0fe55b3e4963b09f
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2023-11-19 20:28:17 +0800

    heteroGNN.py

--- heteroGNN.py
+++ heteroGNN.py
@@ -54,7 +54,7 @@ class heteroGNN(nn.Module):
         otherFeatures = torch.tensor([W, P, N])
         jobFeatures = torch.cat((jobList, otherFeatures.unsqueeze(1).t().expand(jobList.size(0), -1)), dim=1)  # n * 5
 
-        machineFeatures = self.Graph[:, numberOfJobs:]
+        machineFeatures = self.Graph[:, numberOfJobs:]  # n * m
 
         heteroGraph = tensor_to_dgl(self.Graph)
 
@@ -64,7 +64,26 @@ class heteroGNN(nn.Module):
         Graph_conv_re = self.conv(heteroGraph, features)
         job_conv, machine_conv = Graph_conv_re['job'], Graph_conv_re['machine'].flatten()
 
-        return self.linear_for_job(job_conv) + self.linear_for_machine(machine_conv).view(numberOfJobs, -1)
+        Value = self.linear_for_job(job_conv) + self.linear_for_machine(machine_conv).view(numberOfJobs, -1)
+        Possibility_tmp = Value - (1 - self.mask(self.Graph)) * 1e5
+        Possibility = F.softmax(Possibility_tmp.view(-1), dim=0)
+        Possibility = Possibility.view(Possibility_tmp.size())
+
+        return Value, Possibility
+
+
+    def mask(self, Graph: torch.tensor):
+        left = torch.ones((Graph.size()[0], Graph.size()[0]))
+        right = torch.ones((Graph.size()[0], Graph.size()[1]-Graph.size()[0]))
+        row = torch.sum(Graph, dim=1, keepdim=True)
+        col = torch.sum(Graph, dim=0, keepdim=True)
+
+        left = left - row - torch.t(row) - col[:, 0:Graph.size()[0]] - torch.t(col[:, 0:Graph.size()[0]])
+        left = torch.where(left == 1, torch.tensor(1).float(), torch.tensor(0).float())
+        left = left.triu(diagonal=1)
+        right -= row
+
+        return torch.cat((left, right), dim=1)
 
 
 dd = heteroGNN(torch.tensor([[0,1,0,0,0,0],
@@ -72,7 +91,7 @@ dd = heteroGNN(torch.tensor([[0,1,0,0,0,0],
                              [0,0,0,0,0,1],
                              [0,0,0,0,0,1]]), 3, 3)
 
-ret = dd.forward([1,2,3,4],[5,4,3,2],10,15,20)
-print(ret)
+print(dd.mask(dd.Graph))
+print(dd.forward([1,2,3,4],[5,4,3,2],10,15,20))
 
 
