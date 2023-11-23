976caf6cb34d23dd3609dcf9f1afaeb84a1635c0
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2023-11-23 21:13:34 +0800

    修复未完全链接矩阵的bug，增加所有machine连接ternimal节点

--- heteroGNN.py
+++ heteroGNN.py
@@ -21,13 +21,14 @@ def tensor_to_dgl(Graph: torch.tensor):
     g = dgl.heterograph({('job', 'to_job', 'job'): ([], []),
                          ('job', 'to_machine', 'machine'): ([], []),
                          ('machine', 'to_terminal', 'terminal'): ([], [])})
+
     g.add_nodes(numberOfJobs, ntype='job')
     g.add_nodes(numberOfMachines, ntype='machine')
     g.add_nodes(1, ntype='terminal')
 
     g.add_edges(job_to_job_link_src, job_to_job_link_dst, etype=('job', 'to_job', 'job'))
     g.add_edges(job_to_machine_link_src, job_to_machine_link_dst, etype=('job', 'to_machine', 'machine'))
-    g.add_edges(torch.arange(0, numberOfMachines), torch.zeros(numberOfMachines).to(torch.int64), 
+    g.add_edges(torch.arange(0, numberOfMachines), torch.zeros(numberOfMachines).to(torch.int64),
                 etype=('machine', 'to_terminal', 'terminal'))
     print(g)
 
@@ -35,14 +36,14 @@ def tensor_to_dgl(Graph: torch.tensor):
 
 
 class heteroGNN(nn.Module):
-    def __init__(self, numberOfJobs: int, numberOfMachines: int, 
+    def __init__(self, numberOfJobs: int, numberOfMachines: int,
                  job_output_features_number: int, machine_output_features_number: int):
         super(heteroGNN, self).__init__()
-        self.job_output_features_number, self.machine_output_features_number = job_output_features_number, machine_output_features_number
 
         self.conv = dglnn.HeteroGraphConv({
                     'to_job': dglnn.GraphConv(5, job_output_features_number),
-                    'to_machine': dglnn.GraphConv(5, machine_output_features_number)},
+                    'to_machine': dglnn.GraphConv(5, machine_output_features_number),
+                    'to_terminal': dglnn.SAGEConv(5, 1, aggregator_type='pool')},
                     aggregate='max')
 
         """
@@ -61,22 +62,22 @@ class heteroGNN(nn.Module):
         otherFeatures = torch.tensor([W, P, N])
         jobFeatures = torch.cat((jobList, otherFeatures.unsqueeze(1).t().expand(jobList.size(0), -1)), dim=1)  # n * 5
 
-        machineFeatures = self.Graph[:, numberOfJobs:]  # n * m
-
         heteroGraph = tensor_to_dgl(self.Graph)
 
         features = {'job': jobFeatures,
-                    'machine': machineFeatures}
+                    'machine': torch.ones(2, 5),
+                    'terminal': torch.ones(1, 5)}
 
         Graph_conv_re = self.conv(heteroGraph, features)
         job_conv, machine_conv = Graph_conv_re['job'], Graph_conv_re['machine'].flatten()
+        terminal_conv = Graph_conv_re['terminal']
 
         Value = self.linear_for_job(job_conv) + self.linear_for_machine(machine_conv).view(numberOfJobs, -1)
         Possibility_tmp = Value - (1 - self.mask(self.Graph)) * 1e5
         Possibility = F.softmax(Possibility_tmp.view(-1), dim=0)
         Possibility = Possibility.view(Possibility_tmp.size())
 
-        return Value, Possibility
+        return terminal_conv, Value, Possibility
 
 
     def mask(self, Graph: torch.tensor):
@@ -93,19 +94,20 @@ class heteroGNN(nn.Module):
         return torch.cat((left, right), dim=1)
 
 
-dd = heteroGNN(4,2,3,3)
+dd = heteroGNN(4, 2, 20, 20)
 
 
 print(dd.forward(torch.tensor([[0,1,0,0,0,0],
                                [0,0,0,0,1,0],
                                [0,0,0,0,0,1],
                                [0,0,0,0,0,0]]),[1,2,3,4],[5,4,3,2],10,15,20))
+print(dd.mask(dd.Graph))
+print("________________________")
 print(dd.forward(torch.tensor([[0,1,0,0,0,0],
                                [0,0,0,0,1,0],
                                [0,0,0,0,0,1],
                                [0,0,0,0,0,1]]),[1,2,3,4],[5,4,3,2],10,15,20))
 print(dd.mask(dd.Graph))
-
 
 
 
