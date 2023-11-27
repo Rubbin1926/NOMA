8f60e04ee33ad23db15f0bdd23deda1be6556013
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2023-11-27 21:51:32 +0800

    更加直白，哪些卷积参数需要一样

--- HeteroGraphConv_example_code.py
+++ HeteroGraphConv_example_code.py
@@ -8,13 +8,13 @@ g = dgl.heterograph({
 print(g)
 
 conv = dglnn.HeteroGraphConv({
-    'to_job': dglnn.SAGEConv(5, 1, aggregator_type='mean'),
-    'to_machine': dglnn.SAGEConv(5, 2, aggregator_type='mean'),
-    'to_terminal': dglnn.SAGEConv(5, 3, aggregator_type='pool')},
+    'to_job': dglnn.GraphConv(5, 1),
+    'to_machine': dglnn.GraphConv(5, 3),
+    'to_terminal': dglnn.GraphConv(3, 2)},
     aggregate='sum')
 
 h1 = {'job': torch.randn((g.number_of_nodes('job'), 5)),
-      'machine': torch.randn((g.number_of_nodes('machine'), 5)),
-      'terminal': torch.randn((g.number_of_nodes('terminal'), 5))}
+      'machine': torch.randn((g.number_of_nodes('machine'), 3)),
+      'terminal': torch.randn((g.number_of_nodes('terminal'), 1000))}
 h2 = conv(g, h1)
 print(h2)
