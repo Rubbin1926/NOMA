336716b214840eafa44c1feefbc5532d84fb80ff
Zichen Qiu <rubbin2004@hotmail.com>
2023-12-10 18:11:27 +0800

    简单训练了一下，训练的目标是时间value

--- search.py
+++ search.py
@@ -1,6 +1,10 @@
-from env import NOMAenv
-from heteroGNN import heteroGNN
 import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
+import pickle
+from heteroGNN import heteroGNN
+from env import NOMAenv
 import time
 start_time = time.time()
 
@@ -9,9 +13,6 @@ env = NOMAenv()
 observation, info = env.reset(seed=42)
 numberOfJobs = observation.shape[0]
 numberOfMachines = observation.shape[1] - numberOfJobs
-
-gnn = heteroGNN(numberOfJobs, numberOfMachines,
-                job_output_features_number=3, machine_output_features_number=5)
 
 DataSet = {}
 
@@ -32,7 +33,7 @@ def find(Graph, Dataset):
     mask = env.mask(Graph)
 
     if torch.sum(mask) == 0:
-        Dataset[Graph] = (None, Graph, env.calculate_time_dummy(Graph))
+        Dataset[Graph] = (torch.zeros_like(Graph), Graph, env.calculate_time_dummy(Graph))
         return Graph, env.calculate_time_dummy(Graph)
 
     if Graph in Dataset:
@@ -53,12 +54,59 @@ def find(Graph, Dataset):
     Dataset[Graph] = A_star, G_star, V_star
     return G_star, V_star
 
-
+# 3, 16, 136, 1677, 27751, 586018, 15226086
 
 find(observation, DataSet)
 print(DataSet)
+print(f"""len = {len(DataSet)}""")
+
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"程序执行时间：{elapsed_time}秒")
 
+with open('dataset_4_2.pkl', 'wb') as file:
+    pickle.dump(DataSet, file)
 
+
+
+# 从文件中加载字典
+with open('dataset_4_2.pkl', 'rb') as file:
+    GNN_dataset = pickle.load(file)
+
+
+class Read_Dataset(Dataset):
+    def __init__(self, data_dict):
+        self.keys = list(data_dict.keys())
+        self.values = list(data_dict.values())
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        x = self.keys[idx]
+        y = self.values[idx][2]  # 取Value作为y
+        return x, y
+
+
+# 获取关键两个参数
+# first_key = next(iter(GNN_dataset))
+# numberOfJobs = first_key.shape[0]
+# numberOfMachines = first_key.shape[1] - numberOfJobs
+
+Train_dataset = Read_Dataset(GNN_dataset)
+dataloader = DataLoader(Train_dataset, batch_size=8, shuffle=False)
+
+gnn = heteroGNN(numberOfJobs, numberOfMachines,
+                job_output_features_number=3, machine_output_features_number=5)
+optimizer = torch.optim.Adam(gnn.parameters(), lr=0.1)
+loss_f = nn.MSELoss()
+
+for i in range(100):
+    for X, y in Train_dataset:
+        pre_y = gnn.forward(X.type(torch.float32), env.h, env.L, env.W, env.P, env.n)[0]
+        loss = loss_f(pre_y, y.type(torch.float32))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f"""i = {i} loss = {loss}""")
