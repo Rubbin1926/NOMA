41f3a76c3b31c8dff00898bfc5746e217f4b2abf
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2023-12-24 20:57:31 +0800

    改变代码结构，多env训练，para被存储在y的最后一个元素（元组）

--- search.py
+++ search.py
@@ -2,21 +2,11 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
-import pickle
+from torch.utils.data import ConcatDataset
 from heteroGNN import heteroGNN
 from env import NOMAenv
 import time
 start_time = time.time()
-
-
-env_train = NOMAenv()
-env_test = NOMAenv()
-observation, info = env_train.reset(seed=42)
-numberOfJobs = observation.shape[0]
-numberOfMachines = observation.shape[1] - numberOfJobs
-
-trainDataSet = {}
-testDataSet = {}
 
 
 def generate_mask_list(mask):
@@ -35,7 +25,7 @@ def find(Graph, Dataset, env):
     mask = env.mask(Graph)
 
     if torch.sum(mask) == 0:
-        Dataset[Graph] = (torch.zeros_like(Graph), Graph, env.calculate_time_dummy(Graph))
+        Dataset[Graph] = (torch.zeros_like(Graph), Graph, env.calculate_time_dummy(Graph), env.get_parameters())
         return Graph, env.calculate_time_dummy(Graph)
 
     if Graph in Dataset:
@@ -53,35 +43,14 @@ def find(Graph, Dataset, env):
             A_star = action
             G_star = G_
 
-    Dataset[Graph] = A_star, G_star, V_star
+    Dataset[Graph] = (A_star, G_star, V_star, env.get_parameters())
     return G_star, V_star
 
 # 3, 16, 136, 1677, 27751, 586018, 15226086
 
-find(observation, trainDataSet, env_train)
-print(trainDataSet)
-print(f"""len = {len(trainDataSet)}""")
-
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"程序执行时间：{elapsed_time}秒")
-
-# 生成test数据集
-observation, info = env_test.reset(seed=42)
-find(observation, testDataSet, env_test)
-
-
-# with open('trainDataset.pkl', 'wb') as file:
-#     pickle.dump(trainDataSet, file)
-#
-#
-# # 从文件中加载字典
-# with open('trainDataset.pkl', 'rb') as file:
-#     trainDataSet = pickle.load(file)
-
 
 class Read_Dataset(Dataset):
-    def __init__(self, data_dict):
+    def __init__(self, data_dict: dict):
         self.keys = list(data_dict.keys())
         self.values = list(data_dict.values())
 
@@ -91,51 +60,76 @@ class Read_Dataset(Dataset):
     def __getitem__(self, idx):
         x = self.keys[idx]
         y = self.values[idx][2]  # 取Value作为y
-        return x, y
+        parameters = self.values[idx][3]
+        return x, y, parameters
 
 
-def validate(model: heteroGNN, dataset: Read_Dataset, env: NOMAenv):
+def validate(model: heteroGNN, dataset: Read_Dataset):
     cor, num = 0, dataset.__len__()
-    for X, y in dataset:
-        pred = model.forward(X.type(torch.float32), env.h, env.L, env.W, env.P, env.n)[0]
-        if torch.abs(pred - y) / y < 0.1:
+    for X, y, para in dataset:
+        pred = model.forward(X.type(torch.float32), para[0], para[1], para[2], para[3], para[4])[0]
+        if torch.abs(pred - y) / y < 0.2:
             cor += 1
     acc = cor / num
     return acc
 
 
-# 获取关键两个参数
-# first_key = next(iter(GNN_dataset))
-# numberOfJobs = first_key.shape[0]
-# numberOfMachines = first_key.shape[1] - numberOfJobs
+def main():
+
+    # 生成test数据集
+    env_test = NOMAenv()
+    testDataSet = {}
+    observation, info = env_test.reset(seed=42)
+    find(observation, testDataSet, env_test)
+    Test_dataset = Read_Dataset(testDataSet)
+
+    # 获取重要参数
+    numberOfJobs = observation.shape[0]
+    numberOfMachines = observation.shape[1] - numberOfJobs
+
+    train_datasets = []
+    for i in range(3):
+        tmpDataSet = {}
+        env_train = NOMAenv()
+        observation, info = env_train.reset(seed=42)
+        find(observation, tmpDataSet, env_train)
+        tmp_Train_dataset = Read_Dataset(tmpDataSet)
+        train_datasets.append(tmp_Train_dataset)
+
+    Train_dataset = ConcatDataset(train_datasets)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"程序执行时间：{elapsed_time}秒")
+
+    # dataloader = DataLoader(Train_dataset, batch_size=64, shuffle=False)
+
+    gnn = heteroGNN(numberOfJobs, numberOfMachines,
+                    job_output_features_number=32, machine_output_features_number=32)
+    optimizer = torch.optim.Adam(gnn.parameters(), lr=0.0001, weight_decay=1e-6)
+    loss_f = nn.MSELoss()
+
+    for i in range(41):
+        gnn.train()
+        for X, y, para in Train_dataset:
+            pre_y = gnn.forward(X.type(torch.float32), para[0], para[1], para[2], para[3], para[4])[0]
+            loss = loss_f(pre_y, y.type(torch.float32))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f"""i = {i} loss = {loss}""")
+
+        if i % 2 == 0:
+            gnn.eval()
+            acc_train = validate(gnn, Train_dataset)
+            acc_test = validate(gnn, Test_dataset)
+            print(f"### After {i}, acc_train = {acc_train}")
+            print(f"### After {i}, acc_test = {acc_test}")
 
 
-Train_dataset = Read_Dataset(trainDataSet)
-Test_dataset = Read_Dataset(testDataSet)
-dataloader = DataLoader(Train_dataset, batch_size=64, shuffle=False)
-
-gnn = heteroGNN(numberOfJobs, numberOfMachines,
-                job_output_features_number=10, machine_output_features_number=10)
-optimizer = torch.optim.Adam(gnn.parameters(), lr=0.0001, weight_decay=1e-5)
-loss_f = nn.MSELoss()
-
-for i in range(100):
-    gnn.train()
-    for X, y in Train_dataset:
-        pre_y = gnn.forward(X.type(torch.float32), env_train.h, env_train.L, env_train.W, env_train.P, env_train.n)[0]
-        loss = loss_f(pre_y, y.type(torch.float32))
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f"""i = {i} loss = {loss}""")
-
-    if i % 3 == 0:
-        gnn.eval()
-        acc_train = validate(gnn, Train_dataset, env_train)
-        acc_test = validate(gnn, Test_dataset, env_test)
-        print(f"After {i}, acc_train = {acc_train}")
-        print(f"After {i}, acc_tset = {acc_test}")
+if __name__ == "__main__":
+    main()
 
 
  # 加层数
