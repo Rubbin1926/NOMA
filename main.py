25c8b937a55f086fe2262b2708f26d828ba09017
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-05-21 02:01:09 +0800

    加了点参数

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from stable_baselines3 import PPO
 
 env = NOMAenv()
 
-model = PPO("MultiInputPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
+
+model = PPO("MultiInputPolicy", env, verbose=2, learning_rate=1e-6, seed=42, batch_size=32)
+model.learn(total_timesteps=200)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(100):
+for i in range(20):
     action, _states = model.predict(obs, deterministic=True)
-    print(f"""action = {action}""")
+    print("action = ", action)
     obs, reward, done, info = vec_env.step(action)
-    print(f"""obs = {obs}""")
-    print(f"""reward = {reward}""")
+    print("obs = ", obs)
+    print("reward = ", reward)
+
     # VecEnv resets automatically
     if done:
-      obs = env.reset()
+        print("_____________")
+        obs = vec_env.reset()
 
 env.close()
