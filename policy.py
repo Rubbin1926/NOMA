fdbeca6a5d71b2d08e41b23344028eb3c018d2e5
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-07-23 23:20:15 +0800

    随便填了点东西进去

--- policy.py
+++ policy.py
@@ -1,50 +1,44 @@
-from typing import Optional
 import torch
 import torch.nn as nn
-import numpy as np
-import random
-import math
 
-from tensordict.tensordict import TensorDict
-from torchrl.data import (
-    BoundedTensorSpec,
-    CompositeSpec,
-    UnboundedContinuousTensorSpec,
-    UnboundedDiscreteTensorSpec,
-)
-from rl4co.utils.decoding import rollout, random_policy
-from rl4co.envs.common.base import RL4COEnvBase
-from rl4co.envs.common.utils import Generator, get_sampler
-
-from env import numberOfJobs, numberOfMachines, BATCH_SIZE
+from env import numberOfJobs, numberOfMachines
 
 
+# All For Test
 class NOMAInitEmbedding(nn.Module):
     def __init__(self, embed_dim, linear_bias=True):
+        # print("###NOMAInitEmbedding###")
+
         super(NOMAInitEmbedding, self).__init__()
-        node_dim = numberOfJobs
+        node_dim = 1
         self.init_embed = nn.Linear(node_dim, embed_dim, linear_bias)
 
     def forward(self, td):
-        ret = self.init_embed(td["h"])  # For Test
+        bs = td["Graph"].shape[0]
+        ret = self.init_embed(torch.ones(bs, (numberOfJobs*(numberOfJobs + numberOfMachines)), 1).to(td.device))
         return ret
 
 
 class NOMAContext(nn.Module):
     def __init__(self, embed_dim,  linear_bias=True):
+        # print("###NOMAContext###")
+
         super(NOMAContext, self).__init__()
-        pass
+        self.linear = nn.Linear((numberOfJobs*(numberOfJobs + numberOfMachines))*embed_dim, embed_dim, linear_bias)
 
     def forward(self, embeddings, td):
         # embeddings: [batch_size, num_nodes, embed_dim]
         # Output: [batch_size, embed_dim]
-        pass
+
+        embeddings = embeddings.view(embeddings.shape[0], -1)
+        return self.linear(embeddings)
 
 
 class NOMADynamicEmbedding(nn.Module):
     def __init__(self, *args, **kwargs):
+        # print("###NOMADynamicEmbedding###")
+
         super(NOMADynamicEmbedding, self).__init__()
-        pass
 
     def forward(self, td):
-        pass
+        return 0, 0, 0
