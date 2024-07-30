0bcd58bb3b3e5a3303b659b698b3c939eb1b3314
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-07-30 18:52:29 +0800

    新增了tensor_to_dgl()的batch版本

--- policy.py
+++ policy.py
@@ -1,7 +1,26 @@
+import dgl
 import torch
-import torch.nn as nn
+from torch import nn
+import torch.nn.functional as F
+import math
+from dgl.nn import EdgeGATConv
+from torch.distributions import Categorical
+from env import numberOfJobs, numberOfMachines, BATCH_SIZE
 
-from env import numberOfJobs, numberOfMachines
+
+def tensor_to_dgl(Graph: torch.Tensor) -> dgl.DGLGraph:  # 输入进来的tensor with batch是处理成方阵的Graph
+    DGLgraph = dgl.DGLGraph()
+    num_nodes = Graph.shape[0] * (numberOfJobs+numberOfMachines)
+    DGLgraph.add_nodes(num_nodes)
+
+    # 处理为dgl接受的格式
+    indices = torch.nonzero(Graph)
+    indices += (indices[:, 0] * (numberOfJobs+numberOfMachines)).reshape(indices.shape[0], 1)
+    indices = indices[:, 1:].t().contiguous().view(2, -1)
+
+    DGLgraph.add_edges(indices[0], indices[1])
+
+    return DGLgraph
 
 
 # All For Test
@@ -14,6 +33,8 @@ class NOMAInitEmbedding(nn.Module):
         self.init_embed = nn.Linear(node_dim, embed_dim, linear_bias)
 
     def forward(self, td):
+        # Input: td
+        # Output: [batch_size, num_nodes, embed_dim]
         bs = td["Graph"].shape[0]
         ret = self.init_embed(torch.ones(bs, (numberOfJobs*(numberOfJobs + numberOfMachines)), 1).to(td.device))
         return ret
@@ -28,6 +49,7 @@ class NOMAContext(nn.Module):
 
     def forward(self, embeddings, td):
         # embeddings: [batch_size, num_nodes, embed_dim]
+        # td: td
         # Output: [batch_size, embed_dim]
 
         embeddings = embeddings.view(embeddings.shape[0], -1)
