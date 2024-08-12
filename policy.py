38b885e095604ad2ee13bf1472be87b8d852e685
Qiu <131138141+Rubbin1926@users.noreply.github.com>
2024-08-12 19:42:55 +0800

    GNN不需要额外参数。Init目前改动效果不好，导致value不动

--- policy.py
+++ policy.py
@@ -4,7 +4,7 @@ from torch import nn
 import torch.nn.functional as F
 import math
 from dgl.nn import EdgeGATConv
-from env import numberOfJobs, numberOfMachines, BATCH_SIZE
+from env import numberOfJobs, numberOfMachines
 from tensordict.tensordict import TensorDict
 from rl4co.models.rl.common.critic import CriticNetwork
 
@@ -39,7 +39,8 @@ TensorDict(
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def _tensor_to_dgl(Graph: torch.Tensor) -> dgl.DGLGraph:  # 输入进来的tensor with batch是处理成方阵的Graph
+def _tensor_to_dgl(Graph: torch.Tensor, numberOfJobs: int, numberOfMachines: int) -> dgl.DGLGraph:
+    # 输入进来的tensor with batch是处理成方阵的Graph
     """
     用了dgl自带的batch函数
 
@@ -57,7 +58,8 @@ def _tensor_to_dgl(Graph: torch.Tensor) -> dgl.DGLGraph:  # 输入进来的tenso
     return dgl.batch(graph_list)
 
 
-def tensor_to_dgl(Graph: torch.Tensor) -> dgl.DGLGraph:  # 输入进来的tensor with batch是处理成方阵的Graph
+def tensor_to_dgl(Graph: torch.Tensor, numberOfJobs: int, numberOfMachines: int) -> dgl.DGLGraph:
+    # 输入进来的tensor with batch是处理成方阵的Graph
     num_nodes = Graph.shape[0] * (numberOfJobs+numberOfMachines)
 
     # 处理为dgl接受的格式
@@ -85,7 +87,7 @@ class GraphNN(nn.Module):
                                  num_heads=num_heads, allow_zero_in_degree=True)
 
         # self.node_norm = nn.LayerNorm(5, eps=1e-5)
-        self.linear = nn.Linear((numberOfJobs+numberOfMachines)*embed_dim, embed_dim)
+        self.linear = nn.Linear(num_heads * embed_dim, embed_dim)
 
     def forward(self, td: TensorDict) -> torch.Tensor:
         # td: td
@@ -97,11 +99,13 @@ class GraphNN(nn.Module):
         # 使用手动Norm过的数据
         Graph, h, L, W, P, N = td["Graph"], td["norm_h"], td["norm_L"], td["norm_W"], td["norm_P"], td["norm_N"]
         bs = td.batch_size[0]
+        numberOfJobs = h.shape[-1]
+        numberOfMachines = (Graph.shape[-1] // numberOfJobs) - numberOfJobs
         Graph = Graph.reshape(bs, numberOfJobs, numberOfJobs+numberOfMachines)
 
         square_Graph = torch.zeros(bs, numberOfJobs+numberOfMachines, numberOfJobs+numberOfMachines).to(device)
         square_Graph[:, :numberOfJobs, :numberOfJobs + numberOfMachines] = Graph
-        dgl_Graph = tensor_to_dgl(square_Graph)
+        dgl_Graph = tensor_to_dgl(square_Graph, numberOfJobs, numberOfMachines)
 
 
         otherFeatures = torch.cat((W, P, N), dim=1).unsqueeze(1).repeat(1, numberOfJobs, 1)
@@ -133,9 +137,9 @@ class GraphNN(nn.Module):
         snd_time_node_feats = self.conv2(dgl_Graph, fst_time_node_feats.mean(dim=1), edgeFeatures)
         snd_time_node_feats = F.leaky_relu(snd_time_node_feats)
 
-        conv_out = snd_time_node_feats.mean(dim=1).reshape(bs, -1)
-        conv_out = self.linear(conv_out)
-        conv_out = F.leaky_relu(conv_out)
+        embed_dim = snd_time_node_feats.shape[-1]
+        conv_out = self.linear(snd_time_node_feats.reshape(bs*(numberOfJobs+numberOfMachines), -1))
+        conv_out = conv_out.reshape(bs, -1, embed_dim).mean(dim=1)
 
         return conv_out
 
@@ -167,6 +171,52 @@ class MyCriticNetwork(CriticNetwork):
 
 
 # All For Test
+# class NOMAInitEmbedding(nn.Module):
+#     def __init__(self, embed_dim, linear_bias=True):
+#         print("###NOMAInitEmbedding###")
+#         super(NOMAInitEmbedding, self).__init__()
+#
+#         encoder_layer = nn.TransformerEncoderLayer(d_model=8, nhead=2, dim_feedforward=64, dropout=0,
+#                                                    batch_first=True, layer_norm_eps=1e-5)
+#         transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=3)
+#         self.encoder = nn.Sequential(transformer_encoder,
+#                                      nn.Linear(8, embed_dim, linear_bias),
+#                                      nn.LayerNorm(embed_dim, eps=1e-5),
+#                                      nn.LeakyReLU())
+#         self.linear = nn.Sequential(nn.Linear(embed_dim, embed_dim, linear_bias),
+#                                     nn.LayerNorm(embed_dim, eps=1e-5),
+#                                     nn.LeakyReLU())
+#
+#     def forward(self, td: TensorDict) -> torch.Tensor:
+#         # Input: td
+#         # Output: [batch_size, num_nodes, embed_dim]
+#
+#         device = td["Graph"].device
+#         bs = td.batch_size[0]
+#
+#         self.to(device)
+#
+#         h, L, W, P, N = td["norm_h"], td["norm_L"], td["norm_W"], td["norm_P"], td["norm_N"]
+#         numberOfJobs = h.shape[-1]
+#         numberOfMachines = (td["Graph"].shape[-1] // numberOfJobs) - numberOfJobs
+#
+#         feats_tensor = torch.zeros((bs, 8, numberOfJobs+numberOfMachines), device=device)
+#
+#         feats_tensor[:, 0, numberOfJobs:] = 1
+#         feats_tensor[:, 1, :numberOfJobs] = 1
+#
+#         feats_tensor[:, 2, :numberOfJobs] = h
+#         feats_tensor[:, 3, :numberOfJobs] = L
+#
+#         feats_tensor[:, 4, numberOfJobs:] = W
+#         feats_tensor[:, 5, numberOfJobs:] = P
+#         feats_tensor[:, 6, numberOfJobs:] = N
+#
+#         # feats_tensor.shape: [batch_size, numberOfJobs+numberOfMachines, 8]
+#         feats_tensor = feats_tensor.permute(0, 2, 1)
+#
+#         encoder_output = self.encoder(feats_tensor).repeat(1, numberOfJobs, 1)
+#         return self.linear(encoder_output)
 class NOMAInitEmbedding(nn.Module):
     def __init__(self, embed_dim, linear_bias=True):
         print("###NOMAInitEmbedding###")
